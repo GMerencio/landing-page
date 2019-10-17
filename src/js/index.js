4a63a3513c1260afcc4be552c256a75ad6abe57e
Gabriel Merêncio <gmerencio.santos@gmail.com>
2019-10-17 16:52:14 -0300

    Set up smooth scrolling

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,24 @@
+import smoothscroll from 'smoothscroll-polyfill';
 import 'normalize.css';
 import './../scss/main.scss';
+
+/**
+ * Smoothly scrolls to the anchor specified by the link to which this listener
+ * is attached.
+ */
+
+let scrollToAnchor = function(e) {
+  e.preventDefault();
+
+  document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+  });
+}
+
+window.onload = function() {
+  smoothscroll.polyfill();
+  
+  document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+    anchor.addEventListener('click', scrollToAnchor);
+  });
+};
